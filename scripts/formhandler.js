05711ff2c4613e0787bb37cee602302547f208e5
Konnor Gutierrez <kgutierrez1992@live.com>
2021-09-30 16:43:25 -0700

    initial commit

---
+++ scripts/formhandler.js
@@ -0,0 +1,47 @@
+(function(window){ 
+    'use strict';
+    var App = window.App || {};
+    var $ = window.jQuery;
+
+    function FormHandler(selector){
+        if (!selector){
+            throw new Error('no selector provided!');
+        }
+
+        this.$formElement = $(selector);
+        if (this.$formElement === 0){
+            throw new Error('Could not find element with selector: ' + selector);
+        }
+    }
+
+    FormHandler.prototype.addSubmitHandler = function(fn){
+        console.log('Setting submit handler for form.');
+        this.$formElement.on('submit', function (event){
+            event.preventDefault();
+
+            var data = {};
+            $(this).serializeArray().forEach(function (item){
+                data[item.name] = item.value;
+                console.log(item.name + ' is ' + item.value);
+            });
+            console.log(data);
+            fn(data);
+            this.reset();
+            this.elements[0].focus();
+        })
+        //do more things here.
+
+        FormHandler.prototype.addInputHandler = function(fn){
+            console.log('Setting input handler for form');
+            this.$formElement.on('input', '[name="emailAddress"]', function(event){
+                //event handler code here
+                var emailAddress = event.target.value;
+                console.log(fn(emailAddress));
+            }); 
+        }
+    }
+
+    App.FormHandler = FormHandler;
+    window.App = App;
+    
+})(window);
